1a1250c1534e449f3d017a33fbee7b45a5831a83
fhrw <felix.dork@gmail.com>
2022-11-03 13:14:14 +1100

    started building calculation

--- server/controllers/calculation_controller.go
+++ server/controllers/calculation_controller.go
@@ -1,1 +1,13 @@
 package controllers
+
+import (
+	"github.com/fhrw/timetable-server/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func CalculateTimetable(c *gin.Context) {
+	teacherId := c.Param("teacher_id")
+	week := c.Param("week")
+
+}
--- server/models/student.go
+++ server/models/student.go
@@ -6,6 +6,10 @@ type Student struct {
 	First_name string `json:"first_name"`
 	Last_name  string `json:"last_name"`
 	School     string `json:"school"`
+	// considering adding start and end weeks to allow
+	// for students to quit and enrol
+	// Start_week: 0
+	// End_week: -1
 }
 
 type StudentInput struct {
