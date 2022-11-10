d3a51bd58aa84fe0e949ed2ae3c009521282b4c4
fhrw <felix.dork@gmail.com>
2022-11-10 16:37:35 +1100

    constraints fetched based on state week

--- client/src/components/TestControls.js
+++ client/src/components/TestControls.js
@@ -3,21 +3,30 @@ import { connect } from 'react-redux'
 
 import { fetchStudents } from '../actions/studentsActions'
 import { fetchConstraints } from '../actions/constraintsActions'
-import { incrementWeek } from '../actions/uiActions'
+import { decrementWeek, incrementWeek } from '../actions/uiActions'
 
-function TestControls({ dispatch, loading, students, constraints, hasErrors }) {
+function TestControls({ dispatch, loading, students, constraints, ui, hasErrors }) {
 
   useEffect(() => {
     dispatch(fetchStudents())
-    dispatch(fetchConstraints(1))
+    dispatch(fetchConstraints(ui.week))
   }, [dispatch])
 
-  function handle() {
+  function handleInc() {
     dispatch(incrementWeek())
+    dispatch(fetchConstraints(ui.week + 1))
+  }
+
+  function handleDec() {
+    dispatch(decrementWeek())
+    dispatch(fetchConstraints(ui.week - 1))
   }
 
   return (
-    <div><button onClick={handle} /></div>
+    <div>
+      <button onClick={handleInc} >increment</button>
+      <button onClick={handleDec} >decrement</button>
+    </div>
   )
 }
 
@@ -25,7 +34,8 @@ const mapStateToProps = (state) => ({
   students: state.students.students,
   constraints: state.constraints.constraints,
   loading: { students: state.students.loading, constraints: state.constraints.loading },
-  hasErrors: { students: state.students.hasErrors, constraints: state.constraints.hasErrors }
+  hasErrors: { students: state.students.hasErrors, constraints: state.constraints.hasErrors },
+  ui: { week: state.uiData.week }
 })
 
 export default connect(mapStateToProps)(TestControls)
