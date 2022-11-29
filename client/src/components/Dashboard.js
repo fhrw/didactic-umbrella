4c709ad95e7f5aaf44b8be386bd256e364dabfcd
fhrw <felix.dork@gmail.com>
2022-11-30 10:04:08 +1100

    built student display components

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,46 +2,47 @@ import { useEffect } from "react"
 import { connect } from "react-redux"
 //actions
 import { fetchStudents } from '../actions/studentsActions'
-import { fetchTeacher, modifyTeacher } from '../actions/teacherActions'
-import { addConstraint, fetchAddConstraint, fetchConstraints, fetchDelConstraint } from '../actions/constraintsActions'
-import { decrementWeek, incrementWeek, setWeek } from '../actions/uiActions'
-import { fetchHistory, fetchRecalc } from '../actions/historyActions'
-import { fetchSlots, fetchAddSlot, fetchDeleteSlot } from '../actions/slotActions'
+import { fetchTeacher } from '../actions/teacherActions'
+import { fetchConstraints } from '../actions/constraintsActions'
+import { fetchHistory } from '../actions/historyActions'
+import { fetchSlots } from '../actions/slotActions'
 //components
 import WeekNav from "./WeekNav"
 import TeacherControls from "./TeacherControls"
 import Timetable from "./Timetable"
+import StudentDash from "./StudentDash"
 
 function Dashboard({ dispatch, ui, teacher }) {
-  // get students, teacher and histo related to them
-  useEffect(() => {
-    dispatch(fetchStudents())
-    dispatch(fetchTeacher(1))
-    dispatch(fetchHistory(1))
-  }, [dispatch])
+	// get students, teacher and histo related to them
+	useEffect(() => {
+		dispatch(fetchStudents())
+		dispatch(fetchTeacher(1))
+		dispatch(fetchHistory(1))
+	}, [dispatch])
 
-  // get relevant constraints and slots
-  useEffect(() => {
-    dispatch(fetchConstraints(ui.week))
-    dispatch(fetchSlots(teacher.teacher_id, ui.week))
-  }, [dispatch, ui.week])
+	// get relevant constraints and slots
+	useEffect(() => {
+		dispatch(fetchConstraints(ui.week))
+		dispatch(fetchSlots(teacher.teacher_id, ui.week))
+	}, [dispatch, ui.week])
 
-  return <div>
-    <WeekNav />
-    <TeacherControls />
-    <Timetable />
-  </div>
+	return <div>
+		<WeekNav />
+		<TeacherControls />
+		<Timetable />
+		<StudentDash />
+	</div>
 }
 
 const mapStateToProps = (state) => ({
-  students: state.students.students,
-  teacher: state.teacher.teacher,
-  slots: state.slots.slots,
-  constraints: state.constraints.constraints,
-  history: state.history.history,
-  loading: { students: state.students.loading, constraints: state.constraints.loading, teacher: state.teacher.loading, slots: state.slots, loading: state.slots.loading, history: state.history.loading },
-  hasErrors: { students: state.students.hasErrors, constraints: state.constraints.hasErrors },
-  ui: { week: state.uiData.week }
+	students: state.students.students,
+	teacher: state.teacher.teacher,
+	slots: state.slots.slots,
+	constraints: state.constraints.constraints,
+	history: state.history.history,
+	loading: { students: state.students.loading, constraints: state.constraints.loading, teacher: state.teacher.loading, slots: state.slots, loading: state.slots.loading, history: state.history.loading },
+	hasErrors: { students: state.students.hasErrors, constraints: state.constraints.hasErrors },
+	ui: { week: state.uiData.week }
 })
 
 export default connect(mapStateToProps)(Dashboard) 
---
+++ client/src/components/StudentDash.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux'
+
+function StudentDash({ students, constraints, loading, hasErrors }) {
+	if (students.loading) return <p>Loading students</p>
+	if (students.hasErrors) return <p>Error loading students</p>
+	if (!students.length) return <p>No students to display</p>
+	return (
+		<div>
+			{students.map((student) => {
+				const stuCons = constraints.filter((constraint) => constraint.student_id == student.student_id)
+				return Student(student, stuCons)
+			})}
+		</div>
+	)
+}
+
+function Student(student, constraints) {
+	const name = student.first_name + ' ' + student.last_name
+	return (
+		<div>
+			<p>{name}</p>
+			<div>{constraints.map(c => <p>{c.slot}</p>)}</div>
+		</div>
+	)
+}
+
+const mapStateToProps = (state) => ({
+	students: state.students.students,
+	constraints: state.constraints.constraints,
+	loading: { students: state.students.loading, constraints: state.constraints.loading },
+	hasErrors: { students: state.students.hasErrors, constraints: state.constraints.hasErrors }
+
+})
+
+export default connect(mapStateToProps)(StudentDash)
+
