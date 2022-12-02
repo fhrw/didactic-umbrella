fd6e33025e1438531c23ba0a978f1d511bd012ac
fhrw <felix.dork@gmail.com>
2022-12-02 11:49:53 +1100

    built basic controlled form for add student

--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -2,10 +2,16 @@ import { useState } from 'react'
 import { connect } from 'react-redux'
 
 function AddStudent({ dispatch, teacher, loading, hasErrors }) {
+  const [studentInfo, setStudentInfo] = useState({ firstName: "", lastName: "" })
+
+  function handleChange(event) {
+    setStudentInfo({ ...studentInfo, [event.target.name]: event.target.value })
+  }
+
   return (
     <form>
-      <input type="text" />
-      <input type="text" />
+      <input type="text" placeholder='first name' name="firstName" value={studentInfo.firstName} onChange={handleChange} />
+      <input type="text" placeholder='last name' name="lastName" value={studentInfo.lastName} onChange={handleChange} />
     </form>
   )
 }
