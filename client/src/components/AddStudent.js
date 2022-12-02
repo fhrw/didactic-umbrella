62a3d510b77c7a393a7f45eabe66a4020fea9ebd
fhrw <felix.dork@gmail.com>
2022-12-02 15:42:56 +1100

    really fixed the add students bug this time i think

--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { fetchAddStudent } from '../actions/studentsActions'
 
 function AddStudent({ dispatch, teacher, loading, hasErrors }) {
-  const [studentInfo, setStudentInfo] = useState({ teacher_id: teacher.teacher_id, firstName: "", lastName: "", school: "eltham" })
+  const [studentInfo, setStudentInfo] = useState({ teacher_id: "", firstName: "", lastName: "", school: "eltham" })
 
   if (loading) return <p>loading...</p>
   if (hasErrors) return <p>errors...</p>
@@ -16,7 +16,8 @@ function AddStudent({ dispatch, teacher, loading, hasErrors }) {
 
   function handleSubmit(event) {
     event.preventDefault()
-    dispatch(fetchAddStudent(studentInfo))
+    const body = { ...studentInfo, teacher_id: teacher.teacher_id }
+    dispatch(fetchAddStudent(body))
     setStudentInfo({ ...studentInfo, firstName: "", lastName: "" })
   }
 
