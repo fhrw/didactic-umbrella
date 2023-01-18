9a678c3580ea3229f99c254e89da2b76dc6ba933
fhrw <felix.dork@gmail.com>
2023-01-18 12:28:57 +1100

    added context to teacherControls and created slot selector toggle

--- client/src/components/TeacherControls.js
+++ client/src/components/TeacherControls.js
@@ -1,38 +1,51 @@
 import { connect } from "react-redux"
 
+import { DashContext } from './DashContext'
+
 // actions
 import { modifyTeacher } from '../actions/teacherActions'
 import { fetchRecalc } from '../actions/historyActions'
+import SlotPicker from "./SlotPicker"
+import { useContext } from "react"
 
 function TeacherControls({ dispatch, teacher, ui }) {
-	function handleAdd() {
-		const n = { ...teacher, term_length: teacher.term_length + 1 }
-		dispatch(modifyTeacher(teacher.teacher_id, n))
-	}
+  const viewState = useContext(DashContext)
 
-	function handleDel() {
-		const n = { ...teacher, term_length: teacher.term_length - 1 }
-		dispatch(modifyTeacher(teacher.teacher_id, n))
-	}
+  function handleAdd() {
+    const n = { ...teacher, term_length: teacher.term_length + 1 }
+    dispatch(modifyTeacher(teacher.teacher_id, n))
+  }
 
-	function handleCalc() {
-		dispatch(fetchRecalc(teacher.teacher_id, ui.week))
-	}
+  function handleDel() {
+    const n = { ...teacher, term_length: teacher.term_length - 1 }
+    dispatch(modifyTeacher(teacher.teacher_id, n))
+  }
 
-	return (
-		<div>
-			<button onClick={handleDel}>Delete Week</button>
-			<button onClick={handleAdd}>Add Week</button>
-			<button onClick={handleCalc}>Calculate Timetable</button>
-		</div>
-	)
+  function handleCalc() {
+    dispatch(fetchRecalc(teacher.teacher_id, ui.week))
+  }
+
+  function handleToggleSlot() {
+    if (viewState.mode === 'idle') return viewState.setMode('edit_teacher')
+    return viewState.setMode('idle')
+  }
+
+  return (
+    <div>
+      <button onClick={handleDel}>Delete Week</button>
+      <button onClick={handleAdd}>Add Week</button>
+      <button onClick={handleCalc}>Calculate Timetable</button>
+      <button onClick={handleToggleSlot}>Edit Slots</button>
+      {viewState.mode === 'edit_teacher' && <SlotPicker teacher_id={teacher.teacher_id} />}
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => ({
-	teacher: state.teacher.teacher,
-	ui: { week: state.uiData.week },
-	loading: state.teacher.loading,
-	hasError: state.teacher.hasErrors
+  teacher: state.teacher.teacher,
+  ui: { week: state.uiData.week },
+  loading: state.teacher.loading,
+  hasError: state.teacher.hasErrors
 })
 
 export default connect(mapStateToProps)(TeacherControls)
