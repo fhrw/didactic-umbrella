9cf81175ff3016170430e9e37be74ff0f2a2d216
fhrw <felix.dork@gmail.com>
2023-01-19 12:11:25 +1100

    removed comment from context

--- client/src/components/DashContext.js
+++ client/src/components/DashContext.js
@@ -4,9 +4,6 @@ const DashContext = React.createContext()
 
 function DashProvider({ children }) {
   const [mode, setMode] = React.useState("idle")
-  const [studentTarget, setStudentTarget] = React.useState(0)
-  // NOTE: you *might* need to memoize this value
-  // Learn more in http://kcd.im/optimize-context
   const value = { mode, setMode, studentTarget, setStudentTarget }
   return <DashContext.Provider value={value}>{children}</DashContext.Provider>
 }
