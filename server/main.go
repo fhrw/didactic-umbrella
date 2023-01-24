c1ef38efdac41f694b4b819060a028d28ac65337
fhrw <felix.dork@gmail.com>
2023-01-24 16:25:03 +1100

    re activated all slot endpoints

--- server/main.go
+++ server/main.go
@@ -37,9 +37,9 @@ func main() {
 
 	router.POST("/slot", controllers.CreateSlot)
 	router.DELETE("/slot/:slot_id", controllers.DeleteSingleSlot)
-	// router.GET("/slot", controllers.GetAllSlots)
-	// router.GET("/slots/:teacher_id/:week", controllers.GetWeekSlots)
-	// router.GET("/copyprev/:teacher_id/:week", controllers.CopyPrev)
+	router.GET("/slot", controllers.GetAllSlots)
+	router.GET("/slots/:teacher_id/:week", controllers.GetWeekSlots)
+	router.GET("/copyprev/:teacher_id/:week", controllers.CopyPrev)
 
 	// router.GET("/timetable/:teacher_id/:week", controllers.CalculateTimetable)
 
