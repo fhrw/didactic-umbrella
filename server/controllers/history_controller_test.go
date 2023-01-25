a8240dff9b790e401f2b62493badc4812c34219d
fhrw <felix.dork@gmail.com>
2023-01-25 13:57:26 +1100

    lived to fight another day without test

---
+++ server/controllers/history_controller_test.go
@@ -0,0 +1,19 @@
+package controllers
+
+// import (
+// 	"github.com/fhrw/timetable-server/models"
+// 	"github.com/stretchr/testify/require"
+// 	"testing"
+// )
+
+// func TestCreateDayMap(t *testing.T) {
+// 	tests := []struct {
+// 		desc  string
+// 		input []models.History
+// 		want  map[string][]models.History
+// 	}{
+// 		{
+// 			desc: "",
+// 		},
+// 	}
+// }
