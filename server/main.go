a1909044907b617dd071ac40a158c756acda4bde
fhrw <felix.dork@gmail.com>
2023-01-25 17:20:47 +1100

    installed new packages and added back endpoints

--- server/main.go
+++ server/main.go
@@ -13,27 +13,27 @@ func main() {
 
 	models.ConnectDatabase()
 
-	// router.POST("/students", controllers.CreateStudent)
-	// router.GET("/students", controllers.GetAllStudents)
-	// router.GET("/students/:student_id", controllers.GetSingleStudent)
-	// router.PATCH("/students/:student_id", controllers.UpdateStudent)
-	// router.DELETE("/students/:student_id", controllers.DeleteSingleStudent)
-	// router.GET("/students/teacher/:teacher_id", controllers.GetTeachersStudents)
+	router.POST("/students", controllers.CreateStudent)
+	router.GET("/students", controllers.GetAllStudents)
+	router.GET("/students/:student_id", controllers.GetSingleStudent)
+	router.PATCH("/students/:student_id", controllers.UpdateStudent)
+	router.DELETE("/students/:student_id", controllers.DeleteSingleStudent)
+	router.GET("/students/teacher/:teacher_id", controllers.GetTeachersStudents)
 
-	// router.POST("/teacher", controllers.CreateTeacher)
-	// router.GET("/teacher", controllers.GetAllTeachers)
-	// router.PATCH("/teacher/:teacher_id", controllers.UpdateTeacher)
-	// router.GET("/teacher/:teacher_id", controllers.GetSingleTeacher)
+	router.POST("/teacher", controllers.CreateTeacher)
+	router.GET("/teacher", controllers.GetAllTeachers)
+	router.PATCH("/teacher/:teacher_id", controllers.UpdateTeacher)
+	router.GET("/teacher/:teacher_id", controllers.GetSingleTeacher)
 
 	router.POST("/constraints", controllers.CreateConstraint)
-	// router.GET("/constraints", controllers.GetAllConstraints)
-	// router.DELETE("/constraints/:constraint_id", controllers.DeleteSingleConstraint)
-	// router.GET("/constraints/:week", controllers.GetWeekConstraints)
+	router.GET("/constraints", controllers.GetAllConstraints)
+	router.DELETE("/constraints/:constraint_id", controllers.DeleteSingleConstraint)
+	router.GET("/constraints/:week", controllers.GetWeekConstraints)
 
-	// router.POST("/history", controllers.CreateHistory)
-	// router.GET("/history", controllers.GetAllHistory)
-	// router.DELETE("/history/:history_id", controllers.DeleteSingleHistory)
-	// router.GET("/history/:teacher_id", controllers.GetRelevantHistory)
+	router.POST("/history", controllers.CreateHistory)
+	router.GET("/history", controllers.GetAllHistory)
+	router.DELETE("/history/:history_id", controllers.DeleteSingleHistory)
+	router.GET("/history/:teacher_id", controllers.GetRelevantHistory)
 
 	router.POST("/slot", controllers.CreateSlot)
 	router.DELETE("/slot/:slot_id", controllers.DeleteSingleSlot)
@@ -41,7 +41,7 @@ func main() {
 	router.GET("/slots/:teacher_id/:week", controllers.GetWeekSlots)
 	router.GET("/copyprev/:teacher_id/:week", controllers.CopyPrev)
 
-	// router.GET("/timetable/:teacher_id/:week", controllers.CalculateTimetable)
+	router.GET("/timetable/:teacher_id/:week", controllers.CalculateTimetable)
 
 	router.Run("localhost:3000")
 }
