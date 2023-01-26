991e600b42f72c79cb9a8995e0f4c79c4fc1bb09
fhrw <felix.dork@gmail.com>
2023-01-27 10:18:59 +1100

    defined lock structs

---
+++ server/models/lock.go
@@ -0,0 +1,16 @@
+package models
+
+import "github.com/google/uuid"
+
+type Lock struct {
+	Base
+	Student_id uuid.UUID `json:"student_id"`
+	Week       int       `json:"week"`
+	Slot       string    `json:"slot"`
+}
+
+type LockInput struct {
+	Student_id uuid.UUID `json:"student_id" binding:"required"`
+	Week       int       `json:"week" binding:"required"`
+	Slot       string    `json:"slot" binding:"required"`
+}
