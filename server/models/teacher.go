0526d3d1ba6bbeaef93f3c8480978de19fc12df6
fhrw <felix.dork@gmail.com>
2023-01-26 11:46:16 +1100

    added back term length to taecher model

--- server/models/teacher.go
+++ server/models/teacher.go
@@ -2,19 +2,22 @@ package models
 
 type Teacher struct {
 	Base
-	First_name string `json:"first_name"`
-	Last_name  string `json:"last_name"`
-	School     string `json:"school"`
+	First_name  string `json:"first_name"`
+	Last_name   string `json:"last_name"`
+	School      string `json:"school"`
+	Term_length int    `json:"term_length"`
 }
 
 type TeacherInput struct {
-	First_name string `json:"first_name" binding:"required"`
-	Last_name  string `json:"last_name" binding:"required"`
-	School     string `json:"school" binding:"required"`
+	First_name  string `json:"first_name" binding:"required"`
+	Last_name   string `json:"last_name" binding:"required"`
+	School      string `json:"school" binding:"required"`
+	Term_length int    `json:"term_length" binding:"required"`
 }
 
 type TeacherUpdate struct {
-	First_name string `json:"first_name"`
-	Last_name  string `json:"last_name"`
-	School     string `json:"school"`
+	First_name  string `json:"first_name"`
+	Last_name   string `json:"last_name"`
+	School      string `json:"school"`
+	Term_length int    `json:"term_length"`
 }
