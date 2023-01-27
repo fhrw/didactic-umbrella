acfbb8cd7df79dbd9af29ee77c1cb082386a6bf0
fhrw <felix.dork@gmail.com>
2023-01-27 16:50:19 +1100

    finished cases in lock reducer

--- client/src/reducers/lockReducer.js
+++ client/src/reducers/lockReducer.js
@@ -12,5 +12,20 @@ export default function lockReducer(state = initialState, action) {
       return { locks: action.payload, loading: false, hasErrors: false }
     case "getLocksFailure":
       return { ...state, loading: false, hasErrors: false }
+    case "addLock":
+      return { ...state, loading: true, hasErrors: false }
+    case "addLockSuccess":
+      return { locks: [...state.locks, action.payload], loading: false, hasErrors: false }
+    case "addLockFailure":
+      return { ...state, loading: false, hasErrors: false }
+    case "deleteLock":
+      return { ...state, loading: true }
+    case "deleteLockSuccess":
+      const filtered = state.locks.filter(lock => lock.id != action.payload.id)
+      return { locks: filtered, loading: false, hasErrors: false }
+    case "deleteLockFailure":
+      return { ...state, loading: false, hasErrors: true }
+    default:
+      return state
   }
 }
