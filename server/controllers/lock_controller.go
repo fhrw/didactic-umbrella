166bfc6b42ea837694435e9f9084a3e685735ffd
fhrw <felix.dork@gmail.com>
2023-01-27 11:21:08 +1100

    roughly finished CRD funcs

--- server/controllers/lock_controller.go
+++ server/controllers/lock_controller.go
@@ -9,8 +9,8 @@ import (
 )
 
 type LockRequest struct {
-	IDList []uuid.UUID `json:"idList" binding:"required"`
-	Week   int         `json:"week" binding:"required"`
+	Teacher_id uuid.UUID `json:"teacher_id" binding:"required"`
+	Week       int       `json:"week" binding:"required"`
 }
 
 func CreateLock(c *gin.Context) {
@@ -27,13 +27,32 @@ func CreateLock(c *gin.Context) {
 }
 
 func GetLocks(c *gin.Context) {
+	var req LockRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
 
+	var students []models.Student
+	models.DB.Where("teacher_id = ?", req.Teacher_id).Find(&students)
+
+	var locks []models.Lock
+	for _, s := range students {
+		var currLock models.Lock
+		if err := models.DB.Where("student_id = ?", s.ID).Where("week = ?", req.Week).First(&currLock).Error; err != nil {
+			break
+		}
+		locks = append(locks, currLock)
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": locks})
 }
 
 func DeleteLock(c *gin.Context) {
 	var lock models.Lock
-	if err := models.DB.Where("ID = ?", c.Param("lock_id")).First(&lock); err != nil {
+	if err := models.DB.Where("id = ?", c.Param("lock_id")).First(&lock).Error; err != nil {
 		c.AbortWithStatus(http.StatusNotFound)
+		return
 	}
 	models.DB.Delete(&lock)
 	c.JSON(http.StatusOK, gin.H{"data": lock})
