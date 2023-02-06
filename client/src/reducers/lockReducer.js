22da65f17822d1c75bf8e49286e016cd2b9203f8
fhrw <felix.dork@gmail.com>
2023-02-07 10:36:12 +1100

    added filter to lock reducer to delete old lock from state upon accepting new lock

--- client/src/reducers/lockReducer.js
+++ client/src/reducers/lockReducer.js
@@ -1,31 +1,40 @@
 export const initialState = {
   locks: [],
   loading: false,
-  hasErrors: false
-}
+  hasErrors: false,
+};
 
 export default function lockReducer(state = initialState, action) {
   switch (action.type) {
     case "getLocks":
-      return { ...state, loading: true }
+      return { ...state, loading: true };
     case "getLocksSuccess":
-      return { locks: action.payload, loading: false, hasErrors: false }
+      return { locks: action.payload, loading: false, hasErrors: false };
     case "getLocksFailure":
-      return { ...state, loading: false, hasErrors: false }
+      return { ...state, loading: false, hasErrors: false };
     case "addLock":
-      return { ...state, loading: true, hasErrors: false }
+      return { ...state, loading: true, hasErrors: false };
     case "addLockSuccess":
-      return { locks: [...state.locks, action.payload], loading: false, hasErrors: false }
+      const notOld = state.locks.filter(
+        (lock) => lock.student_id !== action.payload.student_id
+      );
+      return {
+        locks: [...notOld, action.payload],
+        loading: false,
+        hasErrors: false,
+      };
     case "addLockFailure":
-      return { ...state, loading: false, hasErrors: false }
+      return { ...state, loading: false, hasErrors: false };
     case "deleteLock":
-      return { ...state, loading: true }
+      return { ...state, loading: true };
     case "deleteLockSuccess":
-      const filtered = state.locks.filter(lock => lock.id != action.payload.id)
-      return { locks: filtered, loading: false, hasErrors: false }
+      const filtered = state.locks.filter(
+        (lock) => lock.id !== action.payload.id
+      );
+      return { locks: filtered, loading: false, hasErrors: false };
     case "deleteLockFailure":
-      return { ...state, loading: false, hasErrors: true }
+      return { ...state, loading: false, hasErrors: true };
     default:
-      return state
+      return state;
   }
 }
