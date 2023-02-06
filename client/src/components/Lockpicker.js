60c39b1a74a90bc36d535914472e2219cef4c89b
fhrw <felix.dork@gmail.com>
2023-02-06 14:01:27 +1100

    built lockpicker component basics

---
+++ client/src/components/Lockpicker.js
@@ -0,0 +1,57 @@
+import { connect } from "react-redux";
+
+import { fetchAddLock, fetchDeleteLock } from "../actions/lockActions";
+function LockPicker({ dispatch, locks, student_id, loading, hasErrors, ui }) {
+  const generic = [
+    ["monday 1", "monday 2", "monday 3", "monday 4", "monday 5", "monday 6"],
+    [
+      "tuesday 1",
+      "tuesday 2",
+      "tuesday 3",
+      "tuesday 4",
+      "tuesday 5",
+      "tuesday 6",
+    ],
+    [
+      "wednesday 1",
+      "wednesday 2",
+      "wednesday 3",
+      "wednesday 4",
+      "wednesday 5",
+      "wednesday 6",
+    ],
+    [
+      "thursday 1",
+      "thursday 2",
+      "thursday 3",
+      "thursday 4",
+      "thursday 5",
+      "thursday 6",
+    ],
+    ["friday 1", "friday 2", "friday 3", "friday 4", "friday 5", "friday 6"],
+  ];
+
+  if (loading) return <p>Loading...</p>;
+  if (hasErrors) return <p>Error...</p>;
+
+  function handleOn(student_id, week, slot) {
+    dispatch(fetchAddLock(student_id, week, slot));
+  }
+
+  function handleOff(lock_id) {
+    dispatch(fetchDeleteLock(lock_id));
+  }
+
+  const lockedSlot = locks.find((l) => {
+    return l.student.id === student_id;
+  });
+}
+
+const mapStateToProps = (state) => ({
+  locks: state.locks.locks,
+  loading: state.locks.loading,
+  hasErrors: state.locks.hasErrors,
+  ui: { week: state.uiData.week },
+});
+
+export default connect(mapStateToProps)(LockPicker);
