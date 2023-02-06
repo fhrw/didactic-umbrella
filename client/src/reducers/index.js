86235fde136e74ffe27ad7976035f69885309809
fhrw <felix.dork@gmail.com>
2023-02-07 10:38:41 +1100

    added lock reducer to the combined reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,21 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
 
-import studentsReducer from './studentsReducer'
-import constraintsReducer from './constraintsReducer'
-import historyReducer from './historyReducer'
-import uiReducer from './uiReducer'
-import teacherReducer from './teacherReducer'
-import slotReducer from './slotReducer'
+import studentsReducer from "./studentsReducer";
+import constraintsReducer from "./constraintsReducer";
+import historyReducer from "./historyReducer";
+import uiReducer from "./uiReducer";
+import teacherReducer from "./teacherReducer";
+import slotReducer from "./slotReducer";
+import lockReducer from "./lockReducer";
 
-const rootReducer = combineReducers({ students: studentsReducer, constraints: constraintsReducer, teacher: teacherReducer, slots: slotReducer, history: historyReducer, uiData: uiReducer })
+const rootReducer = combineReducers({
+  students: studentsReducer,
+  constraints: constraintsReducer,
+  teacher: teacherReducer,
+  slots: slotReducer,
+  locks: lockReducer,
+  history: historyReducer,
+  uiData: uiReducer,
+});
 
-export default rootReducer
+export default rootReducer;
