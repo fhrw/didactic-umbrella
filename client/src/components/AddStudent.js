e0f4f3c01db3e2f4710259fb99847490579954aa
fhrw <felix.dork@gmail.com>
2023-02-17 12:17:29 +1100

    styled addstudent component

--- client/src/components/AddStudent.js
+++ client/src/components/AddStudent.js
@@ -1,44 +1,77 @@
-import { useState } from 'react'
-import { connect } from 'react-redux'
+import { useState } from "react";
+import { connect } from "react-redux";
 
-import { fetchAddStudent } from '../actions/studentsActions'
+import { fetchAddStudent } from "../actions/studentsActions";
 
 function AddStudent({ dispatch, teacher, loading, hasErrors }) {
-  const [studentInfo, setStudentInfo] = useState({ teacher_id: "", firstName: "", lastName: "", school: "eltham" })
+  const [studentInfo, setStudentInfo] = useState({
+    teacher_id: "",
+    firstName: "",
+    lastName: "",
+    school: "eltham",
+  });
 
-  if (loading) return <p>loading...</p>
-  if (hasErrors) return <p>errors...</p>
-
+  if (loading) return <p>loading...</p>;
+  if (hasErrors) return <p>errors...</p>;
 
   function handleChange(event) {
-    setStudentInfo({ ...studentInfo, [event.target.name]: event.target.value })
+    setStudentInfo({ ...studentInfo, [event.target.name]: event.target.value });
   }
 
   function handleSubmit(event) {
-    event.preventDefault()
-    const body = { ...studentInfo, teacher_id: teacher.id }
-    dispatch(fetchAddStudent(body))
-    setStudentInfo({ ...studentInfo, firstName: "", lastName: "" })
+    event.preventDefault();
+    const body = { ...studentInfo, teacher_id: teacher.id };
+    dispatch(fetchAddStudent(body));
+    setStudentInfo({ ...studentInfo, firstName: "", lastName: "" });
   }
 
-  function handleCancel() {
+  function handleCancel() {}
 
-  }
+  const inputStyle = "px-2 py-1 rounded-lg shadow-md";
+  const buttStyle = "px-4 py-2 text-white";
 
   return (
-    <form>
-      <input type="text" placeholder='first name' name="firstName" value={studentInfo.firstName} onChange={handleChange} />
-      <input type="text" placeholder='last name' name="lastName" value={studentInfo.lastName} onChange={handleChange} />
-      <button onClick={handleSubmit}>Submit</button>
-      <button onClick={handleCancel}>Cancel</button>
+    <form className="flex items-center gap-4 bg-neutral-200 p-4 rounded-lg shadow-md self-start">
+      <div className="flex flex-col gap-4">
+        <input
+          className={`${inputStyle}`}
+          type="text"
+          placeholder="first name"
+          name="firstName"
+          value={studentInfo.firstName}
+          onChange={handleChange}
+        />
+        <input
+          className={`${inputStyle}`}
+          type="text"
+          placeholder="last name"
+          name="lastName"
+          value={studentInfo.lastName}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <button
+          className={`${buttStyle} bg-green-400 rounded-l-lg`}
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+        <button
+          className={`${buttStyle} bg-red-400 rounded-r-lg`}
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
+      </div>
     </form>
-  )
+  );
 }
 
 const mapStateToProps = (state) => ({
   teacher: state.teacher.teacher,
   loading: state.teacher.loading,
-  hasErrors: state.teacher.hasErrors
-})
+  hasErrors: state.teacher.hasErrors,
+});
 
-export default connect(mapStateToProps)(AddStudent)
+export default connect(mapStateToProps)(AddStudent);
