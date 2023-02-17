0380592e87f8970ac5e4bee3c0e582fd2a1ad572
fhrw <felix.dork@gmail.com>
2023-02-17 12:27:28 +1100

    rough styling of weeknav

--- client/src/components/WeekNav.js
+++ client/src/components/WeekNav.js
@@ -1,33 +1,41 @@
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 
-import { setWeek } from '../actions/uiActions'
+import { setWeek } from "../actions/uiActions";
 
 function WeekNav({ dispatch, ui, teacher, loading, hasErrors }) {
   function handleInc() {
     if (ui.week < teacher.term_length) {
-      dispatch(setWeek(1))
+      dispatch(setWeek(1));
     }
   }
   function handleDec() {
     if (ui.week > 1) {
-      dispatch(setWeek(-1))
+      dispatch(setWeek(-1));
     }
   }
+
+  const style =
+    "px-2 py-2 bg-neutral-200 rounded-lg text-neutral-600 shadow-md";
+
   return (
     <div className="fixed top-0 w-full h-screen flex items-center px-6 pointer-events-none">
       <div className="w-full flex justify-between pointer-events-auto">
-        <button onClick={handleDec}>prev</button>
-        <button onClick={handleInc}>next</button>
+        <button className={`${style}`} onClick={handleDec}>
+          prev
+        </button>
+        <button className={`${style}`} onClick={handleInc}>
+          next
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 const mapStateToProps = (state) => ({
   teacher: state.teacher.teacher,
   loading: state.teacher.loading,
   hasError: state.teacher.hasErrors,
-  ui: { week: state.uiData.week }
-})
+  ui: { week: state.uiData.week },
+});
 
-export default connect(mapStateToProps)(WeekNav)
+export default connect(mapStateToProps)(WeekNav);
